869010d3becc8112d7d4a7d9869e93ddb9a8f01d
Stefanistkuhl <ghgstefan@gmail.com>
2024-11-08 10:11:27 +0100

    vault backup: 2024-11-08 10:11:27

--- 3.Klasse/itsi/aufgaben/aufgabe2/datein-zum-arbeiten/alias.sh
+++ 3.Klasse/itsi/aufgaben/aufgabe2/datein-zum-arbeiten/alias.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-alias relaunch="sudo sh -c 'docker stop itsi && docker rm itsi && docker buildx build -t itsi:latest . && docker run -d -p 38452:38452 --name itsi itsi:latest && docker exec -it itsi /bin/bash'"
-alias rebuild="sudo sh -c 'docker buildx build -t itsi:latest . && docker run -d -p 38452:38452 --name itsi itsi:latest && docker exec -it itsi /bin/bash'"
-alias stop="sudo sh -c 'docker stop itsi && docker rm itsi'"
+alias relaunch="sh -c 'docker stop itsi && docker rm itsi && docker buildx build -t itsi:latest . && docker run -d -p 38452:38452 --name itsi itsi:latest && docker exec -it itsi /bin/bash'"
+alias rebuild="sh -c 'docker buildx build -t itsi:latest . && docker run -d -p 38452:38452 --name itsi itsi:latest && docker exec -it itsi /bin/bash'"
+alias stop="sh -c 'docker stop itsi && docker rm itsi'"
