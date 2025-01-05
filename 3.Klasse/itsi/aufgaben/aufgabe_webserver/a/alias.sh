284b9768a93bbea41023499bbc5bec94fafb69a9
Stefanistkuhl <ghgstefan@gmail.com>
2025-01-05 21:01:59 +0100

    vault backup: 2025-01-05 21:01:59

--- 3.Klasse/itsi/aufgaben/aufgabe_webserver/a/alias.sh
+++ 3.Klasse/itsi/aufgaben/aufgabe_webserver/a/alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias relaunch="sh -c 'docker stop itsi && docker rm itsi && docker buildx build -t itsi:latest . && docker compose up -d && docker exec -it itsi /bin/bash'"
-alias rebuild="sh -c 'docker buildx build -t itsi:latest . && docker compose up -d && docker exec -it itsi /bin/bash'"
+alias relaunch="sh -c 'docker stop itsi && docker rm itsi && ./build.sh && docker compose up -d && docker exec -it itsi /bin/bash'"
+alias rebuild="sh -c './build.sh && docker compose up -d && docker exec -it itsi /bin/bash'"
 alias stop="sh -c 'docker compose down'"
 
