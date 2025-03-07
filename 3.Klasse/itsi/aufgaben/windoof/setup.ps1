1b8c18fce88d6b53fcff22628f05d73231a1c413
Stefanistkuhl <ghgstefan@gmail.com>
2025-03-07 13:49:54 +0100

    a

--- 3.Klasse/itsi/aufgaben/windoof/setup.ps1
+++ 3.Klasse/itsi/aufgaben/windoof/setup.ps1
@@ -4,7 +4,7 @@ $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-file C:\
 $Trigger = New-ScheduledTaskTrigger -AtStartup
 $Settings = New-ScheduledTaskSettingsSet
 Register-ScheduledTask -TaskName "after-setup" -Action $Action -Trigger $Trigger -Settings $Settings
-$url = "https://raw.githubusercontent.com/Stefanistkuhl/obsidianschule/46ae36051d646a7110d6473d6ec8ab69456b1829/3.Klasse/itsi/aufgaben/windoof/script.ps1"
+$url = "https://raw.githubusercontent.com/Stefanistkuhl/obsidianschule/refs/heads/main/3.Klasse/itsi/aufgaben/windoof/script.ps1"
 $dest = "C:\Users\Administrator\script.ps1"
 Invoke-WebRequest -Uri $url -OutFile $dest
 Restart-Computer
